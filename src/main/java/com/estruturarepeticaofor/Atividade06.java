f706a96e46b6520f402badde66864ef50720cbac
gabriel_b_nandi <gabrielnandi003@gmail.com>
2025-07-29 21:49:16 -0300

    atualização

---
+++ src/main/java/com/estruturarepeticaofor/Atividade06.java
@@ -0,0 +1,16 @@
+package main.java.com.estruturarepeticaofor;
+
+import java.util.Scanner;
+
+public class Atividade06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int numero = scanner.nextInt();
+
+        for (int i = numero; i >=1  ; --i) {
+                System.out.println(i);
+        }
+    }
+}
