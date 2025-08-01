269c8edc4ce024adbb5b62c56fda7e0bbbc19366
gabriel_b_nandi <gabrielnandi003@gmail.com>
2025-07-31 21:42:19 -0300

    atualização

---
+++ src/main/java/com/estruturarepeticaofor/Atividade07.java
@@ -0,0 +1,18 @@
+package main.java.com.estruturarepeticaofor;
+
+import java.util.Scanner;
+
+public class Atividade07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int numero = scanner.nextInt();
+        int soma = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            soma = soma * i;
+        }
+        System.out.println("Fatorial: " + soma);
+    }
+}
